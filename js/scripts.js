c7a30792c4f778e27a8e035393fa72e79d1dc8dc
Kihara-tony <tonykiharatonkin6@gmail.com>
2019-05-11 12:16:29 +0300

    added javascript of play ,about ,rules and instruction and a marquee.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,85 @@
+$(document).ready(function(){
+  $(".about").click(function(){
+    $(".ab").show();
+    $(".about").hide();
+  });
+  $('.ab').click(function(){
+    $(".ab").hide();
+    $(".about").show();
+  })
+  $(".instruct").click(function(){
+    jQuery(".ins").show();
+    jQuery(".instruct").hide();
+  });
+  $(".ins").click(function(){
+    jQuery(".instruct").show();
+    jQuery(".ins").hide();
+  });
+  $(".rules").click(function(){
+    jQuery(".rul").show();
+    jQuery(".rules").hide();
+  });
+  $(".rul").click(function(){
+    jQuery(".rules").show();
+    jQuery(".rul").hide();
+  });
+  $("#game").click(function(){
+    var player1=prompt("Enter name of the first player:")
+    var player2=prompt("Enter name of the second player:")
+    $(".player1").text(player1);
+    $(".player2").text(player2);
+  });
+});
+
 function player(name,roll,total,active) {
   this.name = name;
   this.roll =0;
   this.total=0;
   this.active=active;
 }
-var game =function(){
-  var name=prompt("Enter name of the first player:");
-  var player2=prompt("Enter name of the second player:");
+
+function next(){
+  active ===1 || active ===0;
+
 }
- $(document).ready(function(){
-   $(".player1").text(player1);
-   $(".player2").text(player2);
-   // new game play button
-   $("#submit").click(function(){
-     init()
-   })
-  $(".about img").click(function(){
-    $(".about").slideToggle();
-    $(".ab").slideToggle();
-  });
-  $(".instruct").click(function(){
-    $(".instruct").slideToggle();
-    $(".ins").slideToggle();
-  });
-  $(".rules").click(function(){
-    $(".rules").slideToggle();
-    $(".rul").slideToggle();
-  })
-});
+//  $(document).ready(function(){
+//
+//
+//    // new game play button
+//    $("#submit").click(function(){
+//      init();
+//    });
+//
+//    // roll button
+//    $("#roll").click(function(){
+//      if(playing){
+//        rolls =math.floor(math.random()*6)+1;
+//      }
+//      if(rolls !==1){
+//        roll += rolls;
+//        $(".insert" + active).text(roll);
+//      }
+//      else{
+//        next();
+//      }
+//    })
+//
+//    // hold button
+//    $("#hold").click(function(){
+//      if(playing){
+//        total[active] +=roll;
+//        $(".remove" + active).text(total[active]);
+//      }
+//    //
+//    //   // winner
+//    //   if(total[active]>=15){
+//    //     $(".winner" + active).text(Winner!);
+//    //     playing=false;
+//    //   }
+//    //   else{
+//    //     next();
+//    //   }
+//    // })
+//
+// });
+// })
