09e7fae6e78523f2da7e6e11077c668c96d8f542
Kihara-tony <tonykiharatonkin6@gmail.com>
2019-05-11 08:46:46 +0300

    added javascript of new game play button .

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
-$(document).ready(function(){
-  $("#game").click(function(){
-    $(".about").toggle();
-    $(".ab").toggle();
+function player(name,roll,total,active) {
+  this.name = name;
+  this.roll =0;
+  this.total=0;
+  this.active=active;
+}
+var game =function(){
+  var name=prompt("Enter name of the first player:");
+  var player2=prompt("Enter name of the second player:");
+}
+ $(document).ready(function(){
+   $(".player1").text(player1);
+   $(".player2").text(player2);
+   // new game play button
+   $("#submit").click(function(){
+     init()
+   })
+  $(".about img").click(function(){
+    $(".about").slideToggle();
+    $(".ab").slideToggle();
+  });
+  $(".instruct").click(function(){
+    $(".instruct").slideToggle();
+    $(".ins").slideToggle();
+  });
+  $(".rules").click(function(){
+    $(".rules").slideToggle();
+    $(".rul").slideToggle();
   })
-})
+});
