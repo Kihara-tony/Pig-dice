2c2014db22288e750c2bb6a6e736e1d8be99fa34
Kihara-tony <tonykiharatonkin6@gmail.com>
2019-05-13 14:41:13 +0300

    added javascript of hold and roll button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,39 @@
+function guess(){
+  var six = Math.floor(Math.random()*6)+1;
+  return six;
+}
+function Player(){
+  this.cumulative = 0;
+  this.total = 0;
+}
+player1 = new Player();
+player2 = new Player();
+
+Player.prototype.roll = function(){
+  var random = guess();
+  if (random === 1){
+    this.cumulative = 0;
+    alert("You rolled a 1,Next players turn");
+  }
+  else {
+    this.cumulative += random;
+  }
+  return random;
+}
+Player.prototype.held = function(){
+  this.total += this.cumulative;
+  this.cumulative = 0;
+  return this.total;
+}
+Player.prototype.win = function(){
+  if(player1.total >= 100){
+    alert("player1 has won !!!!!!!!!!!");
+  }
+  else if (player2.total >= 100) {
+    alert("player2 has won !!!!!!!!!!!!")
+  }
+};
+
 $(document).ready(function(){
   $(".about").click(function(){
     $(".ab").show();
@@ -29,47 +65,22 @@ $(document).ready(function(){
     var player2=prompt("Enter name of the second player:")
     $(".player2").text(player2);
   });
-  var active = function(){
-    if(player1.active ===true && player2.active ===false){
-      $(".one").prop("disabled",false);
-      $(".two").prop("disabled",true);
-    }
-    else {
-      $(".one").prop("disabled",true);
-      $(".two").prop("disabled",false);
-    };
-  };
-  function player(name,roll,total,active,playing) {
-    this.roll =0;
-    this.total=0;
-    this.active=active;
-    playing = true;
-  }
-  function user() {
-    if (player1.active === true && player2.active === false) {
-        $('.one').children().prop('disabled', false);
-        $('.one').removeClass('disableGamingArea');
-        $('.two').children().prop('disabled', true);
-        $('.two').addClass('disableGamingArea');
-    } else {
-        $('.one').children().prop('disabled', true);
-        $('.one').addClass('disableGamingArea');
-        $('.two').children().prop('disabled', false);
-        $('.two').removeClass('disableGamingArea');
-    }
-};
-
-  // roll button
-  function guess(){
-    return Math.floor((Math.random()*6)+1);
-    user();
-  }
-  $("#roll").click(function(){
-    $(".insert").text(guess());
+  $("#roll1").click(function(){
+    $(".roll1").text(player1.roll());
+    $(".insert1").text(player1.cumulative);
   });
-  // function total(){
-  //   return total += guess;
-  // }
-  // $("#hold").click(function(){
-  //   $(".remove").text(total());
+  $("#roll2").click(function(){
+    $(".roll2").text(player2.roll());
+    $(".insert2").text(player2.cumulative);
+  });
+  $("#hold1").click(function(){
+    $(".remove1").text(player1.held());
+    player1.win();
+    alert("Next player's turn!!");
+  });
+  $("#hold2").click(function(){
+    $(".remove2").text(player2.held());
+    player2.win();
+    alert("Next player's turn!!");
+  });
 });
